2d07004334a52961b8796036af81d5e96773b88f
Vahan Grigoryan <vahan.grigoryan@gmail.com>
2023-01-18 10:26:30 +0300

    -

--- templates/module/src/{class-name}/bin/{module-name}-bundle.sh
+++ templates/module/src/{class-name}/bin/{module-name}-bundle.sh
@@ -6,4 +6,9 @@ then
     CURDIR=`pwd`
 fi
 cd $CURDIR
-cd ../web && /usr/bin/php index.php $1 /bundle.json
+if [ "$2" = 'bundle' ]
+then
+    cd ../web && /usr/bin/php index.php $1 /modules/{module-name}/bundle.json
+else
+    cd ../web && /usr/bin/php index.php $1 /bundle.json
+fi

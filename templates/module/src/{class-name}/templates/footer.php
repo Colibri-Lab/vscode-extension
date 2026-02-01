941a1f79a9e282965cb01e12ae2fbd6e1392da18
Vahan P. Grigoryan <vahan.grigoryan@gmail.com>
2026-02-01 11:19:23 +0400

    -

--- templates/module/src/{class-name}/templates/footer.php
+++ templates/module/src/{class-name}/templates/footer.php
@@ -6,11 +6,18 @@ if (App::$domainKey === '{module-name}') {
     <script>
         App.InitializeApplication(
             '{module-name}',
+            1,
             Colibri.Web.Router.RouteOnHistory,
             Colibri.IO.Request.RequestEncodeTypeEncrypted,
             true,
-            false,
-            location.protocol + '//' + location.host
+            true,
+            location.protocol + '//' + location.host,
+            'en-US',
+            'en-US',
+            {
+                code: 'USD',
+                symbol: '$'
+            }
         );
     </script>
 <?php

ded1ea464e3f33b9890d0c136d94eba82faeb15a
Vahan P. Grigoryan <vahan.grigoryan@gmail.com>
2026-09-08 12:28:18 +0400

    -

--- templates/module/src/{class-name}/templates/header.php
+++ templates/module/src/{class-name}/templates/header.php
@@ -1,3 +1,11 @@
-<title>{class-name}</title>
-<meta name="google" content="notranslate" />
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<?php
+use Colibri\App;
+
+if (App::$domainKey === '{module-name}') {
+    ?>
+    <title>{class-name}</title>
+    <meta name="google" content="notranslate" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<?php
+}
+?>
